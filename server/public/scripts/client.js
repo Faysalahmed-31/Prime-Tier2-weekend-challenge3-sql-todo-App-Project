4b6dc5af2a2125ee2c7574e06a6b4c4691c30e9e
Faisal Abdulle <Faysalyare-31@hotmail.com>
2021-10-17 14:45:12 -0500

    adding client POST

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,26 @@
 console.log('JQ');
+
+$(document).ready(onReady);
+
+
+function onReady() {
+    console.log('ready!');
+    
+    $('#submitBtn').on('click', postTasks);
+}
+
+
+function postTasks() {
+    let tasksObject = {
+        task: $('#taskInput').val(),
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/list',
+        data: tasksObject
+    }).then( function (response) {
+        $('#taskInput').val(''),
+        $('#taskInput').focus();
+        getTasks();
+    });
+}
