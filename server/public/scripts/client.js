29e525fc9df594d90ccf4a02835580b286a3c9ea
Faisal Abdulle <Faysalyare-31@hotmail.com>
2021-10-17 14:58:16 -0500

    adding GET file append to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,10 @@
-console.log('JQ');
+//console.log('JQ');
 
-$(document).ready(onReady);
-
+$( document ).ready(onReady)
 
 function onReady() {
     console.log('ready!');
-    
+    getTasks();
     $('#submitBtn').on('click', postTasks);
 }
 
@@ -22,5 +21,27 @@ function postTasks() {
         $('#taskInput').val(''),
         $('#taskInput').focus();
         getTasks();
+    });
+}
+
+
+function getTasks() {
+    $.ajax({
+        method: 'GET',
+        url: '/list'
+    }).then(function (response) {
+        console.log("get response", response);
+
+        //empty list
+        $('#displayList').empty();
+        // append data to the DOM
+        for (let i = 0; i < response.length; i++) {
+            $('#displayList').append(`
+                <li>${response[i].task}
+                    <button class="completeBtn" data-id="${response[i].id}">Complete</button>
+                    <button class="deleteBtn" data-id="${response[i].id}">Delete</button>
+                </li>
+            `);
+        }
     });
 }
