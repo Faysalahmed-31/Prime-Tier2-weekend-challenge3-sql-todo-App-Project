866089daf893335ccec17e6a3ac36f716d001ee9
Faisal Abdulle <Faysalyare-31@hotmail.com>
2021-10-17 14:30:14 -0500

    adding GET route in the server

--- server/routes/router.js
+++ server/routes/router.js
@@ -5,4 +5,32 @@ const app = express();
 
 
 // DB CONNECTION
-const pool = require( '../modules/pool.js' );
+const pool = require( '../modules/pool' );
+
+// GET
+router.get('/', (req, res) => {
+    let queryString = `
+        SELECT * FROM "todo_app"
+    `;
+    pool.query(queryString)
+        .then((results) => {
+            // log the response data
+            console.log(results.rows);
+            
+            // send the db results
+            // to the client
+            res.send(results.rows);
+        })
+        .catch((err) => {
+            console.log('SQL GET failed ', err);
+            res.sendStatus(500);
+        });
+});
+
+
+module.exports = router;
+
+
+
+
+
